a7746c3e3ea7c595a2596358f03c343ac1636096
Allan Ly <allanvly@gmail.com>
2018-03-06 18:38:46 -0800

    firebase key

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,20 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAjaLqoIK09KzJg774l7pamA3adaufuSHI",
+    authDomain: "stayfly-bucketlist.firebaseapp.com",
+    databaseURL: "https://stayfly-bucketlist.firebaseio.com",
+    projectId: "stayfly-bucketlist",
+    storageBucket: "stayfly-bucketlist.appspot.com",
+    messagingSenderId: "196059567979"
+  };
+ 
+  firebase.initializeApp(config);
+
+  var database = database.firebase()
+
+  var users = database.ref('/users')
+
+
 /* */
 /* */
 /* */
