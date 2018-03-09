0480cc54bd1579f8fd8ff1a02445a277b919f8ca
Allan Ly <allanvly@gmail.com>
2018-03-08 18:25:14 -0800

    first page complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 $(".submitBTN").on('click', function () {
   $("#content").empty()
+  $(".userInput").hide()
   var querySearch = $("#eventSearch").val().trim()
   var queryLocation = $("#locationSearch").val().trim()
   var queryDate = "future"
@@ -11,16 +12,17 @@ $(".submitBTN").on('click', function () {
     for (i = 0; i < 10; i++) {
       console.log(queryParse.events.event[i].title)
       console.log(queryParse.events.event[i])
-      var div = $("#content")
-      var title = "<h5>" + (i + 1) + ". " + queryParse.events.event[i].title + "</h5>"
+      var div = $('<div>')
+      var title = "<h5>" + (i + 1) + ". " + queryParse.events.event[i].title + "</h5><hr>"
       var description = "<p>" + queryParse.events.event[i].description + "</p>"
       var startTime = "<p>" + queryParse.events.event[i].start_time + "</p>"
       var date = moment(startTime, 'YYYY-MM-DD HH:mm:ss').format('MMM D, YYYY h:mm a')
       var eventfulLink = "<p><a href='" + queryParse.events.event[i].url + "'target='" + "_blank'>" + "Event Link" + "</a></p>"
       var image = $('<img>')
+      image.addClass('eventImg')
       div.append(title)
-      div.append(description + "<br>")
       div.append(date + "<br><br>")
+      div.append("<b>Description: </b><br><br>" + description + "<br>")
       
       if (queryParse.events.event[i].image === null) {
       }
@@ -28,7 +30,9 @@ $(".submitBTN").on('click', function () {
         var imageURL = image.attr('src', "http:" + queryParse.events.event[i].image.url)
         div.append(imageURL)
       }
-      div.append(eventfulLink + "<br><hr>")
+      div.append(eventfulLink + "<br>")
+      div.addClass('card-panel teal lighten-5 boxsize')
+      $("#content").append(div)
     }
   })
   return false
