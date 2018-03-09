f4ec70d672b3fcf6e9cc438a4b869e9d56bfc94c
Allan Ly <allanvly@gmail.com>
2018-03-08 21:09:23 -0800

    googleapi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+
 $(".submitBTN").on('click', function () {
   $("#content").empty()
   $(".userInput").hide()
@@ -10,29 +11,36 @@ $(".submitBTN").on('click', function () {
   }).then(function (response) {
     var queryParse = JSON.parse(response)
     for (i = 0; i < 10; i++) {
-      console.log(queryParse.events.event[i].title)
-      console.log(queryParse.events.event[i])
-      var div = $('<div>')
-      var title = "<h5>" + (i + 1) + ". " + queryParse.events.event[i].title + "</h5><hr>"
-      var description = "<p>" + queryParse.events.event[i].description + "</p>"
-      var startTime = "<p>" + queryParse.events.event[i].start_time + "</p>"
-      var date = moment(startTime, 'YYYY-MM-DD HH:mm:ss').format('MMM D, YYYY h:mm a')
-      var eventfulLink = "<p><a href='" + queryParse.events.event[i].url + "'target='" + "_blank'>" + "Event Link" + "</a></p>"
-      var image = $('<img>')
-      image.addClass('eventImg')
-      div.append(title)
-      div.append(date + "<br><br>")
-      div.append("<b>Description: </b><br><br>" + description + "<br>")
-      
-      if (queryParse.events.event[i].image === null) {
+
+      console.log(queryParse)
+      if (queryParse.events === null) {
+        $("<body>").append("<h1>Sorry no events around you</h1>")
       }
       else {
-        var imageURL = image.attr('src', "http:" + queryParse.events.event[i].image.url)
-        div.append(imageURL)
+        console.log(queryParse.events.event[i])
+        console.log(queryParse.events.event[i].title)
+        var div = $('<div>')
+        var title = "<h5>" + (i + 1) + ". " + queryParse.events.event[i].title + "</h5><hr>"
+        var description = "<p>" + queryParse.events.event[i].description + "</p>"
+        var startTime = "<p>" + queryParse.events.event[i].start_time + "</p>"
+        var date = moment(startTime, 'YYYY-MM-DD HH:mm:ss').format('MMM D, YYYY h:mm a')
+        var eventfulLink = "<p><a href='" + queryParse.events.event[i].url + "'target='" + "_blank'>" + "Event Link" + "</a></p>"
+        var image = $('<img>')
+        image.addClass('eventImg')
+        div.append(title)
+        div.append(date + "<br><br>")
+        div.append("<b>Description: </b><br><br>" + description + "<br>")
+
+        if (queryParse.events.event[i].image === null) {
+        }
+        else {
+          var imageURL = image.attr('src', "http:" + queryParse.events.event[i].image.url)
+          div.append(imageURL)
+        }
+        div.append(eventfulLink + "<br>")
+        div.addClass('card-panel teal lighten-5 boxsize')
+        $("#content").append(div)
       }
-      div.append(eventfulLink + "<br>")
-      div.addClass('card-panel teal lighten-5 boxsize')
-      $("#content").append(div)
     }
   })
   return false
@@ -40,3 +48,6 @@ $(".submitBTN").on('click', function () {
   //       console.log(err)
 })
 
+
+  // <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGKzK10pnII1YoVFkujW3oEbCJaiCLwrs&callback=initMap"
+  //   async defer></script>
